b29cde24aae30365f9bf8eee21bda0d3ba7afc02
lephucminhkhoa <khoa.lpm.67khmt@ntu.edu.vn>
2025-11-17 02:02:51 +0700

    Create BT2_Tinh cac gia tri.py

---
+++ BT2_Tinh cac gia tri.py
@@ -0,0 +1,9 @@
+# Tính giá trị các biểu thức
+a = (15 + 3) * 2 - 10 / 5
+b = 2 ** 3 + 4 * 2
+c = "Python" * 2 + " " + "3.10"
+
+# In ra kết quả và kiểu dữ liệu
+print("a =", a, "Kiểu dữ liệu của a:", type(a))
+print("b =", b, "Kiểu dữ liệu của b:", type(b))
+print("c =", c, "Kiểu dữ liệu của c:", type(c))
