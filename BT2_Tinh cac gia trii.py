e427c7d4f0898f337b6b29b108a9503b49d2a115
lephucminhkhoa <khoa.lpm.67khmt@ntu.edu.vn>
2025-11-17 02:07:24 +0700

    Bai tap 2

---
+++ BT2_Tinh cac gia trii.py
@@ -0,0 +1,6 @@
+a = (15 + 3) * 2 - 10 / 5
+b = 2 ** 3 + 4 * 2
+c = "Python" * 2 + " " + "3.10"
+print("a =", a, "Kiểu dữ liệu của a:", type(a))
+print("b =", b, "Kiểu dữ liệu của b:", type(b))
+print("c =", c, "Kiểu dữ liệu của c:", type(c))
---
+++ BT2_tinh ban kinh.py
@@ -0,0 +1,5 @@
+PI = 3.14159
+SÓ_NGÀY_TRONG_TUẦN = 7
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+chu_vi = 2 * PI * ban_kinh
+print("Chu vi hình tròn là:", chu_vi)
