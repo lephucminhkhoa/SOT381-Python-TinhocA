f4df50e0f73d3f607249cc134f62ffc4e1dfb527
lephucminhkhoa <khoa.lpm.67khmt@ntu.edu.vn>
2025-11-17 02:02:01 +0700

    Create BT2_Khaibao.py

---
+++ BT2_Khaibao.py
@@ -0,0 +1,5 @@
+# Khai báo các biến
+ho_ten = "Le Phuc Minh Khoa"  # Họ tên sinh viên
+tuoi = 18  # Tuổi sinh viên
+diem_trung_binh = 8.5  # Điểm trung bình
+trang_thai = "Đậu"  # Trạng thái đậu/môn
